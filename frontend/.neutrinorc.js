85529b6ff6ed1ad550560c7f53a38c7f0750c59d
David Evans <davidje13@users.noreply.github.com>
2019-03-03 19:15:44 +0000

    Disable linter when using webpack dev server due to annoying bugs (false positives); run only during testing

--- frontend/.neutrinorc.js
+++ frontend/.neutrinorc.js
@@ -1,33 +1,42 @@
+const linter = ['@neutrinojs/airbnb', {
+  eslint: {
+    rules: {
+      'arrow-parens': ['error', 'always'],
+      'import/no-named-as-default': ['off'], // conflicts with redux convention
+      'jsx-a11y/anchor-is-valid': ['error', {
+        'components': ['Link'],
+        'specialLink': ['to'],
+      }],
+      'operator-linebreak': ['error', 'after'],
+      'react/destructuring-assignment': ['off'], // conflicts with redux actions
+      'react/jsx-one-expression-per-line': ['off'], // too buggy
+    },
+    baseConfig: {
+      overrides: [{
+        files: ['**/test-helpers/*'],
+        rules: {
+          'import/no-extraneous-dependencies': ['error', {
+            'devDependencies': true,
+          }],
+        },
+      }],
+    },
+  },
+}];
+
 module.exports = {
   options: {
     tests: 'src',
   },
   use: [
-    ['@neutrinojs/airbnb', {
-      eslint: {
-        rules: {
-          'arrow-parens': ['error', 'always'],
-          'import/no-named-as-default': ['off'], // conflicts with redux convention
-          'jsx-a11y/anchor-is-valid': ['error', {
-            'components': ['Link'],
-            'specialLink': ['to'],
-          }],
-          'operator-linebreak': ['error', 'after'],
-          'react/destructuring-assignment': ['off'], // conflicts with redux actions
-          'react/jsx-one-expression-per-line': ['off'], // too buggy
-        },
-        baseConfig: {
-          overrides: [{
-            files: ['**/test-helpers/*'],
-            rules: {
-              'import/no-extraneous-dependencies': ['error', {
-                'devDependencies': true,
-              }],
-            },
-          }],
-        },
-      },
-    }],
+    (neutrino) => {
+      // exclude linting from development as it is very buggy (lots of false
+      // positives) when used with webpack dev server, but works fine in
+      // isolation
+      if (process.env.NODE_ENV !== 'development') {
+        neutrino.use(linter);
+      }
+    },
     ['@neutrinojs/copy', {
       patterns: [{
         context: 'src/static',
