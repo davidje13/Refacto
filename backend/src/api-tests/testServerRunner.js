13011528678f5d111060598cbc24a18c28f48e27
David Evans <davidje13@users.noreply.github.com>
2019-05-12 11:27:55 +0100

    Simplify test server runner

--- backend/src/api-tests/testServerRunner.js
+++ backend/src/api-tests/testServerRunner.js
@@ -1,19 +1,13 @@
 export default (serverFn) => {
   let server;
 
-  beforeEach(async () => {
-    server = await serverFn();
-    if (server.createServer) {
-      server = server.createServer();
-    }
-  });
-
   beforeEach((done) => {
-    if (server) {
-      server.listen(0, done);
-    } else {
-      done();
-    }
+    server = null;
+    Promise.resolve(serverFn())
+      .then((rawServer) => {
+        server = rawServer.listen(0, done);
+      })
+      .catch(done.fail);
   });
 
   afterEach((done) => {
