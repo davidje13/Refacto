8d881ed616f69072595543b39223e5e372c9161f
David Evans <davidje13@users.noreply.github.com>
2019-06-28 20:20:51 +0100

    Improve typescript config

--- backend/.neutrinorc.js
+++ backend/.neutrinorc.js
@@ -3,45 +3,25 @@ const copy = require('@neutrinojs/copy');
 const jest = require('@neutrinojs/jest');
 const node = require('@neutrinojs/node');
 const { baseRules, testRules, tsRules } = require('../eslint.js');
-
-const typescript = () => (neutrino) => {
-  const { extensions } = neutrino.options;
-  const index = extensions.indexOf('js');
-  extensions.splice(index, 0, 'ts', 'tsx');
-  neutrino.options.extensions = extensions;
-};
+const { typescriptBefore, typescriptAfter } = require('./neutrino-typescript');
 
 module.exports = {
   options: {
     tests: 'src',
   },
   use: [
-    typescript(),
+    typescriptBefore(),
     (neutrino) => neutrino.use(airbnb({
       eslint: {
-        parser: '@typescript-eslint/parser',
-        parserOptions: {
-          project: './tsconfig.json',
-        },
-        plugins: ['@typescript-eslint'],
         rules: Object.assign({}, baseRules, tsRules),
         baseConfig: {
           extends: [
             'plugin:eslint-comments/recommended',
-            'plugin:@typescript-eslint/eslint-recommended',
-            'plugin:@typescript-eslint/recommended',
           ],
           overrides: [{
             files: ['**/test-helpers/*', '**/*.test.*'],
             rules: Object.assign({}, tsRules, testRules),
           }],
-          settings: {
-            'import/resolver': {
-              node: {
-                extensions: neutrino.options.extensions.map((ext) => `.${ext}`),
-              },
-            },
-          },
         },
       },
     })),
@@ -61,14 +41,13 @@ module.exports = {
               node: '10.15',
             },
           }],
-          ['@babel/preset-typescript', {}],
         ],
         plugins: [
           'transform-dynamic-import',
-          '@babel/plugin-proposal-class-properties',
         ],
       },
     }),
+    typescriptAfter(),
     (neutrino) => neutrino.config.stats('minimal'),
   ],
 };
--- backend/src/types/json-immutability-helper/index.d.ts
+++ backend/src/types/json-immutability-helper/index.d.ts
@@ -1,76 +1,66 @@
 declare module 'json-immutability-helper' {
-  type Condition<T> = object;
+  interface ConditionValue<T> {
+    equals?: T;
+    greaterThanOrEqual?: T;
+    lessThanOrEqual?: T;
+    greaterThan?: T;
+    lessThan?: T;
+    not?: T;
+  }
+
+  interface ConditionKeyValue<K, V> extends ConditionValue<V> {
+    key: K;
+  }
+
+  type ConditionKey<T, K extends keyof T> = ConditionKeyValue<K, T[K]> | [K, T[K]];
+
+  type ConditionEntry<T> = ConditionValue<T> | ConditionKey<T, keyof T>;
+
+  type Condition<T> = ConditionEntry<T> | ConditionEntry<T>[];
 
   export type Spec<T> = (
     T extends string ? StringSpec :
       T extends number ? NumberSpec :
         T extends ((infer U)[]) ? ArraySpec<U> :
           ObjectSpec<T>
-  ) | {
-    $set: T;
-  } | {
-    $updateIf: [Condition<T>, Spec<T>, Spec<T>?];
-  } | {
-    $seq: [...Spec<T>[]];
-  };
+  ) |
+  { $set: T } |
+  { $updateIf: [Condition<T>, Spec<T>, Spec<T>?] } |
+  { $seq: [...Spec<T>[]] };
 
   interface StringSpec {
     $replaceAll: [string, string];
   }
 
-  type NumberSpec = {
-    $add: number;
-  } | {
-    $subtract: number;
-  } | {
-    $multiply: number;
-  } | {
-    $divide: number;
-  } | {
-    $reciprocal: number;
-  };
+  type NumberSpec =
+    { $add: number } |
+    { $subtract: number } |
+    { $multiply: number } |
+    { $divide: number } |
+    { $reciprocal: number };
 
-  type ArraySpec<T> = {
-    $push: T[];
-  } | {
-    $unshift: T[];
-  } | {
-    $splice: ([number, number?] | [number, number, ...T[]])[];
-  } | {
-    $insertBeforeFirstWhere: [Condition<T>, ...T[]];
-  } | {
-    $insertAfterFirstWhere: [Condition<T>, ...T[]];
-  } | {
-    $insertBeforeLastWhere: [Condition<T>, ...T[]];
-  } | {
-    $insertAfterLastWhere: [Condition<T>, ...T[]];
-  } | {
-    $updateAll: Spec<T>;
-  } | {
-    $updateWhere: [Condition<T>, Spec<T>];
-  } | {
-    $updateFirstWhere: [Condition<T>, Spec<T>];
-  } | {
-    $updateLastWhere: [Condition<T>, Spec<T>];
-  } | {
-    $deleteWhere: Condition<T>;
-  } | {
-    $deleteFirstWhere: Condition<T>;
-  } | {
-    $deleteLastWhere: Condition<T>;
-  } | {
-    [index: number]: Spec<T>;
-  };
+  type ArraySpec<T> =
+    { $push: T[] } |
+    { $unshift: T[] } |
+    { $splice: ([number, number?] | [number, number, ...T[]])[] } |
+    { $insertBeforeFirstWhere: [Condition<T>, ...T[]] } |
+    { $insertAfterFirstWhere: [Condition<T>, ...T[]] } |
+    { $insertBeforeLastWhere: [Condition<T>, ...T[]] } |
+    { $insertAfterLastWhere: [Condition<T>, ...T[]] } |
+    { $updateAll: Spec<T> } |
+    { $updateWhere: [Condition<T>, Spec<T>] } |
+    { $updateFirstWhere: [Condition<T>, Spec<T>] } |
+    { $updateLastWhere: [Condition<T>, Spec<T>] } |
+    { $deleteWhere: Condition<T> } |
+    { $deleteFirstWhere: Condition<T> } |
+    { $deleteLastWhere: Condition<T> } |
+    { [index: number]: Spec<T> };
 
-  type ObjectSpec<T> = {
-    $toggle: (keyof T)[];
-  } | {
-    $unset: (keyof T)[];
-  } | {
-    $merge: Partial<T>;
-  } | {
-    [K in keyof T]?: Spec<T[K]>;
-  };
+  type ObjectSpec<T> =
+    { $toggle: (keyof T)[] } |
+    { $unset: (keyof T)[] } |
+    { $merge: Partial<T> } |
+    { [K in keyof T]?: Spec<T[K]> };
 
   type DirectiveFn<T> = (param: any, old: T) => T;
   type ConditionFn<T> = (param: T) => (actual: T) => boolean;
