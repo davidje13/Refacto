686073f4414ff9dba1033b8f7c40b6263f1547c1
David Evans <davidje13@users.noreply.github.com>
2019-06-29 11:17:13 +0100

    Convert config loader to typescript

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,9 +2,12 @@ import baseConfig from './config/default.json';
 
 // Reads configuration from environment variables (falling back to values in
 // config/default.json if not set)
-// Variables are read in snake_case and exported in camelCase
+// Variables are read in UPPER_SNAKE_CASE and exported in camelCase
 
-function getEnv(name, def = null) {
+function getEnv(name: string, def: null): string | null;
+function getEnv<T>(name: string, def: T): T;
+
+function getEnv(name: string, def: any): any {
   const value = process.env[name];
   if (value === undefined) {
     return def;
@@ -19,24 +22,27 @@ function getEnv(name, def = null) {
   return value;
 }
 
-function makeSnake(name) {
+function makeSnake(name: string): string {
   return name.replace(/([a-z])([A-Z])/g, '$1_$2');
 }
 
-function makeCamel(name) {
-  return name.replace(/_(.)/g, (m, c) => c.toUpperCase());
+function makeCamel(name: string): string {
+  return name.replace(/_(.)/g, (m, c): string => c.toUpperCase());
 }
 
-function populateConfig(base, env = '') {
+function populateConfig<T>(base: T, env = ''): T {
   if (!base || typeof base !== 'object') {
     return getEnv(env, base);
   }
   const envPrefix = env ? `${env}_` : '';
 
-  const result = {};
-  Object.keys(base).forEach((key) => {
-    result[makeCamel(key)] = populateConfig(
-      base[key],
+  const result: T = base;
+  Object.keys(base).forEach((key): void => {
+    if (makeCamel(key) !== key) {
+      throw new Error(`Property ${key} should be camel case`);
+    }
+    result[key as keyof T] = populateConfig(
+      base[key as keyof T],
       `${envPrefix}${makeSnake(key).toUpperCase()}`,
     );
   });
