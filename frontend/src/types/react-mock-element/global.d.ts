37c7ace78eaadd9cd7467fdb64f272841371cde4
David Evans <davidje13@users.noreply.github.com>
2019-08-03 14:54:08 +0100

    Extract react-mock-element types into module

--- frontend/src/types/react-mock-element/global.d.ts
+++
@@ -1,7 +0,0 @@
-import 'react';
-
-declare global {
-  interface HTMLElement {
-    mockProps: Record<string, any>;
-  }
-}
--- frontend/src/types/react-mock-element/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'react-mock-element' {
-  import React from 'react';
-
-  export default function (name: string): () => React.ReactElement;
-}
