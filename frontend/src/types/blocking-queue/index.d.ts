2194f8795638d282d3f30aa12c9dc9854d706d89
David Evans <davidje13@users.noreply.github.com>
2019-08-03 15:04:11 +0100

    Add all types for blocking-queue package

--- frontend/src/types/blocking-queue/index.d.ts
+++ frontend/src/types/blocking-queue/index.d.ts
@@ -1,7 +1,24 @@
 declare module 'blocking-queue' {
-  export default class BlockingQueue<T> {
+  class BlockingQueue<T> {
     public push(value: T): void;
 
     public pop(): Promise<T>;
+
+    public size(): number;
   }
+
+  namespace BlockingQueue {
+    export class QueueConsumer<T> {
+      public constructor(queue: BlockingQueue<T>);
+
+      public start(
+        consumer: (value: T) => (Promise<void> | void),
+        concurrency?: number,
+      ): void;
+
+      public end(): Promise<void>;
+    }
+  }
+
+  export default BlockingQueue;
 }
