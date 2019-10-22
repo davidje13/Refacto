705d56afacc665bd31e2c3ec69a9d1c01180fa3c
David Evans <davidje13@users.noreply.github.com>
2019-10-22 16:48:56 +0100

    Minor formatting

--- src/frontend/src/api/UserTokenService.ts
+++ src/frontend/src/api/UserTokenService.ts
@@ -7,15 +7,12 @@ export default class UserTokenService {
     service: string,
     externalToken: string,
   ): Promise<string> {
-    const response = await fetch(
-      `${this.apiBase}/sso/${service}`,
-      {
-        method: 'POST',
-        cache: 'no-cache',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ externalToken }),
-      },
-    );
+    const response = await fetch(`${this.apiBase}/sso/${service}`, {
+      method: 'POST',
+      cache: 'no-cache',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ externalToken }),
+    });
     const body = await response.json();
     if (response.status >= 300 || body.error) {
       throw new Error(body.error || 'Connection failed');
