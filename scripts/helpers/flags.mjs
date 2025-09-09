3640145030bccd029cb651ad1c46ee177526c64e
David Evans <davidje13@users.noreply.github.com>
2025-09-09 11:51:40 +0100

    Tests can now use --throw-deprecation (since express 5), experiment with permissions model (add notes but still disabled)

--- scripts/helpers/flags.mjs
+++ scripts/helpers/flags.mjs
@@ -1,4 +1,12 @@
 const COMMON_FLAGS = [
+  // TODO: enabling --permission seems risky:
+  // https://nodejs.org/docs/latest/api/permissions.html#permission-model-constraints
+  // "OpenSSL engines cannot be requested at runtime when the Permission Model is enabled, affecting the built-in crypto, https, and tls modules"
+  // Likely to affect tokens and DB access? Can this be fixed by setting up --openssl-config correctly?
+  // Also "Run-Time Loadable Extensions cannot be loaded when the Permission Model is enabled, affecting the sqlite module."
+  // Could the limits be OS-dependent?
+  //'--permission',
+  //'--allow-fs-read=./*', // permissions model: required to load sources + static files. TODO: should be relative to script, not CWD (see https://github.com/nodejs/node/issues/59820)
   '--force-node-api-uncaught-exceptions-policy',
   '--no-addons',
   '--disallow-code-generation-from-strings',
@@ -10,11 +18,10 @@ export const LOCAL_RUNTIME_FLAGS = [
   ...COMMON_FLAGS,
   '--disable-proto=throw',
   '--pending-deprecation',
+  //'--allow-child-process', // permissions model: required for rollup's watch mode. TODO: suppress warning (see https://github.com/nodejs/node/issues/59818)
 ];
 
 export const TEST_RUNTIME_FLAGS = [
   ...LOCAL_RUNTIME_FLAGS,
-
-  // TODO: find what is bringing in node:punycode module
-  //'--throw-deprecation',
+  '--throw-deprecation',
 ];
