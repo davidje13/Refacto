ab6fec761e501fd34afaf3f1ed3367de0535f52a
David Evans <davidje13@users.noreply.github.com>
2026-01-27 14:26:16 +0000

    Suppress view transition errors

--- frontend/src/components/RetroRouter.tsx
+++ frontend/src/components/RetroRouter.tsx
@@ -211,10 +211,11 @@ function useRetroReducer(retroId: string | null): RetroReducerState {
           ? 'archive'
           : null;
         if (animation) {
-          document.startViewTransition({
+          const viewTransition = document.startViewTransition({
             update: () => flushSync(() => setRetroState(setter)),
             types: [animation],
           });
+          viewTransition.ready.catch(() => {}); // ignore errors (e.g. 'Skipped ViewTransition due to document being hidden')
         } else {
           setRetroState(setter);
         }
