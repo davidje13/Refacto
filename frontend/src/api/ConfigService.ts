aee742e600c7f49c2bcb71f99c9f78ed94317556
David Evans <davidje13@users.noreply.github.com>
2026-02-17 15:20:59 +0000

    Fix config preloading in Safari (work around browser bug)

--- frontend/src/api/ConfigService.ts
+++ frontend/src/api/ConfigService.ts
@@ -5,6 +5,16 @@ export class ConfigService {
   constructor(private readonly apiBase: string) {}
 
   get(): Promise<ClientConfig> {
-    return jsonFetch(`${this.apiBase}/config`, {});
+    return jsonFetch(`${this.apiBase}/config`, {
+      priority: 'high',
+
+      // Safari's preload handling is a bit fragile / broken, and does not
+      // understand crossorigin="anonymous". These settings have been found
+      // to support preloading in both Chrome and Safari. In practice, the
+      // URL is always same-origin, and we do not have any cookies anyway,
+      // so credentials:include is safe.
+      credentials: 'include',
+      mode: 'no-cors',
+    });
   }
 }
