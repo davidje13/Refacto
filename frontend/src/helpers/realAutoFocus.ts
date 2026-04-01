7a45ef0f09bddcc84156c684f2ce053137fdeb2f
David Evans <davidje13@users.noreply.github.com>
2026-04-01 01:06:06 +0100

    Various mobile tweaks, always use explicit reference viewport for sizes

--- frontend/src/helpers/realAutoFocus.ts
+++ frontend/src/helpers/realAutoFocus.ts
@@ -2,5 +2,15 @@
 // is problematic in some situations (e.g. in <dialog>). This helper can be
 // used as a `ref` to set the real autofocus property in these cases.
 
-export const realAutoFocus = (o: HTMLElement | null) =>
-  o?.setAttribute('autofocus', '');
+export const realAutoFocus = (o: HTMLElement | null) => {
+  if (o) {
+    if (
+      o.getAttribute('type') === 'date' &&
+      matchMedia?.('(pointer: coarse)').matches
+    ) {
+      // avoid auto-focusing date fields on mobile, as it will make the date picker UI appear immediately
+      return;
+    }
+    o.setAttribute('autofocus', '');
+  }
+};
